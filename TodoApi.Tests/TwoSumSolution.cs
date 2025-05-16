aeee6b92cd208d06abc1074fca5b28b25dfa1b20
Maheen Chowdhury <maheen.mn.c@gmail.com>
2025-05-16 22:57:31 +0600

    add twosum

---
+++ TodoApi.Tests/TwoSumSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi;
+public class TwoSumSolution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (dict.TryGetValue(complement, out int index))
+            {
+                return new[] { index, i };
+            }
+            dict[nums[i]] = i;
+        }
+        return Array.Empty<int>();
+    }
+}
---
+++ TodoApi.Tests/TwoSumSolutionTest.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace TodoApi;
+public class TwoSumSolutionTests
+{
+    private readonly TwoSumSolution _twoSumSolution = new();
+
+    [Fact]
+    public void NormalCase_FindsPair()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = _twoSumSolution.TwoSum(nums, target);
+
+        Assert.Equal(new[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void EdgeCase_EmptyArray_ReturnsEmpty()
+    {
+        int[] nums = { };
+        int target = 0;
+        int[] result = _twoSumSolution.TwoSum(nums, target);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void DuplicateNumbers_FindsCorrectIndices()
+    {
+        int[] nums = { 3, 3 };
+        int target = 6;
+        int[] result = _twoSumSolution.TwoSum(nums, target);
+
+        Assert.Equal(new[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void NoSolution_ReturnsEmpty()
+    {
+        int[] nums = { 1, 2, 3 };
+        int target = 7;
+        int[] result = _twoSumSolution.TwoSum(nums, target);
+
+        Assert.Empty(result);
+    }
+}
